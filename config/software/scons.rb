08f765c07938871a78e4d5f01c8ff7064fd13a06
AJ Christensen <aj@junglist.io>
2013-07-16 12:42:00 +1200

    [scons] typo

--- config/software/scons.rb
+++ config/software/scons.rb
@@ -12,7 +12,7 @@ embedded = File.join(install_dir, 'embedded')
 lib_dir = File.join(embedded, 'lib')
 include_dir = File.join(embedded, 'include')
 
-env(
+env = (
   'LDFLAGS' => "-L#{lib_dir} -I#{include_dir}",
   'CFLAGS' => "-L#{lib_dir} -I#{include_dir}",
   'LD_RUN_PATH' => lib_dir,
